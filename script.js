04e77e94020792c42f859962ccb945c6c72c86de
Sara Szwejkowska <saraszw@op.pl>
2022-09-05 22:49:45 +0200

    mentoring

---
+++ script.js
@@ -0,0 +1,15 @@
+{
+    const abc="a"; // 4 bity 1 bit 0 1
+    let c="33";
+    // null i undefined
+    const tab=[1,2,3]
+    tab.push(4)
+}
+const a=3;
+console.log(typeof a) // number
+alert()
+prompt()
+confirm()
+
+//Zad 1.
+//Jakie mamy sposoby na tworzenie kodu w JS? Z którego skorzystasz, gdy potrzebować będziesz na szybko przetestować snippet kodu? 
